cf7624d04bc89a71b6f855cb295d58f855b53ba4
vine-sea <vine-sea@qq.com>
2020-09-13 11:36:53 +0800

    sec

--- fun.js
+++ fun.js
@@ -3,6 +3,7 @@
 function scro(params){
 var c=document.getElementsByClassName('L')
 var c2=document.getElementsByClassName('S')
+var c3=document.getElementsByClassName('T')
     if(params.scrollY>50){
         log('here')
         
@@ -11,13 +12,14 @@ var c2=document.getElementsByClassName('S')
     c[0].style.backgroundColor='#1e90ff'
     c[0].style.filter = "grayscale(50%) opcity(90%)";
     c2[0].style.visibility='visible'
+    c3[0].style.marginTop= scrollY+25+'px'
     return
     }
     // c[0].style.visibility='visible'
     // c[0].style.height='100px'
     c[0].style.backgroundColor='#70a1ff'
     c2[0].style.visibility='hidden'
-
+    c3[0].style.marginTop= 25+'px'
 }
 
 function log(params) {
